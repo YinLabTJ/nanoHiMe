71ec15154e24ad40335fe327102f1ade8ff37e77
Xie Zhiyuan <xzy_aq@aliyun.com>
2022-11-02 13:15:43 +0800

    rm example

--- example/modification_example.sh
+++
@@ -1,3 +0,0 @@
-../nanoHiMe mCG ref_upper.fa example.eventalign.txt.gz test1.mCG
-../nanoHiMe 6mA ref_upper.fa example.eventalign.txt.gz test2.6mA 50 25 test.peak.bed
-../nanoHiMe 6mA ref_upper.fa example.eventalign.txt.gz test3.6mA 50 25
