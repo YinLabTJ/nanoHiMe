27f07939e89363de786c7a9f5d800698d15cc2d6
Xie Zhiyuan <xzy_aq@aliyun.com>
2022-11-02 12:04:53 +0800

    Add example

---
+++ example/model_training_example.sh
@@ -0,0 +1,9 @@
+../Model_training \
+	-m mcg \
+	-c 2 \
+	-r /data1/workdata/xiezhy/Nanopore/fast5/pipeline/pipeline_20221002/example/ref_upper.fa \
+	-e /data1/workdata/xiezhy/Nanopore/fast5/pipeline/pipeline_20221002/example/example.eventalign.txt.gz \
+	-o /data1/workdata/xiezhy/Nanopore/fast5/pipeline/pipeline_20221002/example/pipe_test_2rounds \
+	-g 2 \
+	-p /data1/workdata/xiezhy/Nanopore/fast5/pipeline/pipeline_20221002/example/EM_shift.txt \
+	-i 500
